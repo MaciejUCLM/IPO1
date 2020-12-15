ee2b192436948ff1ddd8d2c6336c5e8e5acffa80
Maciej Nalepa <maciej.nalepa@alu.uclm.es>
2020-12-15 11:36:39 +0100

    Tree node adding

--- practicas/CampingGestion/src/presentation/PanelAccomodation.java
+++ practicas/CampingGestion/src/presentation/PanelAccomodation.java
@@ -7,22 +7,33 @@ import java.awt.BorderLayout;
 import javax.swing.JSplitPane;
 import javax.swing.JTree;
 import javax.swing.JPanel;
+import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.TreePath;
+import javax.swing.tree.TreeSelectionModel;
+import javax.swing.tree.DefaultMutableTreeNode;
+import java.awt.ComponentOrientation;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.event.TreeSelectionListener;
+import javax.swing.event.TreeSelectionEvent;
 
 public class PanelAccomodation extends MainPanel {
 
+	private static IAppWindow main;
 	private static JButton[] tools;
+	private JTree tree;
 
 	/**
 	 * Create the panel.
 	 */
 	public PanelAccomodation() {
-		tools = new JButton[4];
-		tools[2] = new JButton("New reservation");
-		tools[1] = new JButton("New object");
-		tools[0] = new JButton("New category");
-		tools[3] = new JButton("Delete");
+		tools = new JButton[3];
+		tools[0] = new JButton("New reservation");
+		tools[1] = new JButton("New category");
+		tools[1].addActionListener(new NewNodeActionListener());
+		tools[2] = new JButton("Delete");
 		// TODO
-		//tools[0].addActionListener(new LogoutActionListener());
 		//tools[0].setIcon(new ImageIcon(PanelAccount.class.getResource("/presentation/flags/polish.png")));
 
 		setLayout(new BorderLayout(0, 0));
@@ -30,7 +41,31 @@ public class PanelAccomodation extends MainPanel {
 		JSplitPane splitPane = new JSplitPane();
 		add(splitPane, BorderLayout.CENTER);
 		
-		JTree tree = new JTree();
+		DefaultTreeModel treeModel = new DefaultTreeModel(
+			new DefaultMutableTreeNode("Camping") {
+				{
+					DefaultMutableTreeNode node_1;
+					node_1 = new DefaultMutableTreeNode("Bungalows");
+						node_1.add(new DefaultMutableTreeNode("Deluxe"));
+						node_1.add(new DefaultMutableTreeNode("Standard"));
+					add(node_1);
+					node_1 = new DefaultMutableTreeNode("Fields");
+						node_1.add(new DefaultMutableTreeNode("Small"));
+						node_1.add(new DefaultMutableTreeNode("Medium"));
+						node_1.add(new DefaultMutableTreeNode("Big"));
+						node_1.add(new DefaultMutableTreeNode("Caravan"));
+					add(node_1);
+				}
+			}
+		);
+		
+		tree = new JTree();
+		tree.addTreeSelectionListener(new TreeTreeSelectionListener());
+		tree.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+		tree.setEditable(true);
+		tree.setModel(treeModel);
+		tree.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);;
+		tree.setCellRenderer(new CampingTreeCellRenderer());
 		splitPane.setLeftComponent(tree);
 		
 		JPanel panel = new JPanel();
@@ -48,4 +83,42 @@ public class PanelAccomodation extends MainPanel {
 		// TODO Auto-generated method stub
 	}
 
+	private class TreeTreeSelectionListener implements TreeSelectionListener {
+		public void valueChanged(TreeSelectionEvent arg0) {
+			if (main == null)
+				main = IAppWindow.getController().getWindow(EnumWindows.MAIN);
+			main.log("Selected node: " + arg0.getPath());
+		}
+	}
+
+	public DefaultMutableTreeNode addObject(Object child) {
+	    DefaultMutableTreeNode parentNode = null;
+	    TreePath parentPath = tree.getSelectionPath();
+
+	    if (parentPath == null) {
+	        //There is no selection. Default to the root node.
+	        parentNode = (DefaultMutableTreeNode) (tree.getModel().getRoot());
+	    } else {
+	        parentNode = (DefaultMutableTreeNode) (parentPath.getLastPathComponent());
+	    }
+
+	    return addObject(parentNode, child, true);
+	}
+	
+	public DefaultMutableTreeNode addObject(DefaultMutableTreeNode parent, Object child, boolean shouldBeVisible) {
+		DefaultMutableTreeNode childNode = new DefaultMutableTreeNode(child);
+		((DefaultTreeModel) tree.getModel()).insertNodeInto(childNode, parent, parent.getChildCount());
+
+		//Make sure the user can see the lovely new node.
+		if (shouldBeVisible)
+			tree.scrollPathToVisible(new TreePath(childNode.getPath()));
+
+		return childNode;
+	}
+
+	private class NewNodeActionListener implements ActionListener {
+		public void actionPerformed(ActionEvent arg0) {
+			addObject("Test Object");
+		}
+	}
 }
