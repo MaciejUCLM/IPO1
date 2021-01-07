6856bc5e8dde7ed908089e5e92862158507f8934
Maciej Nalepa <maciej.nalepa@alu.uclm.es>
2021-01-07 12:07:52 +0100

    Accomodation object editing

--- practicas/CampingGestion/src/presentation/PanelAccomodation.java
+++ practicas/CampingGestion/src/presentation/PanelAccomodation.java
@@ -41,11 +41,18 @@ import javax.swing.DefaultComboBoxModel;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
+import javax.swing.event.ChangeListener;
+import javax.swing.event.ChangeEvent;
+import java.awt.event.ItemListener;
+import java.awt.event.ItemEvent;
+import javax.swing.event.CaretListener;
+import javax.swing.event.CaretEvent;
 
 public class PanelAccomodation extends MainPanel {
 	
+	private AccomodationTreeNode selectedNode;
+	
 	private JTree tree;
-
 	private JTextField txtNode = new JTextField();
 	private ManagerTable table;
 	private JLabel lblTags;
@@ -86,48 +93,47 @@ public class PanelAccomodation extends MainPanel {
 		
 		JSplitPane splitPane = new JSplitPane();
 		add(splitPane, BorderLayout.CENTER);
-		
+
 		tree = new JTree();
 		tree.setMinimumSize(new Dimension(20, 0));
 		tree.setPreferredSize(new Dimension(150, 64));
 		tree.addTreeSelectionListener(new TreeTreeSelectionListener());
 		tree.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
 		tree.setEditable(true);
-		tree.setModel(new DefaultTreeModel(
-			new DefaultMutableTreeNode("Camping") {
+		selectedNode = new AccomodationTreeNode("Camping") {
 				{
 					DefaultMutableTreeNode node_1;
 					DefaultMutableTreeNode node_2;
-					node_1 = new DefaultMutableTreeNode("Bungalows");
-						node_2 = new DefaultMutableTreeNode("Deluxe");
-							node_2.add(new DefaultMutableTreeNode("A"));
-							node_2.add(new DefaultMutableTreeNode("B"));
+					node_1 = new AccomodationTreeNode("Bungalows");
+						node_2 = new AccomodationTreeNode("Deluxe");
+							node_2.add(new AccomodationTreeNode("A"));
+							node_2.add(new AccomodationTreeNode("B"));
 						node_1.add(node_2);
-						node_2 = new DefaultMutableTreeNode("Standard");
-							node_2.add(new DefaultMutableTreeNode("A"));
-							node_2.add(new DefaultMutableTreeNode("B"));
-							node_2.add(new DefaultMutableTreeNode("C"));
+						node_2 = new AccomodationTreeNode("Standard");
+							node_2.add(new AccomodationTreeNode("A"));
+							node_2.add(new AccomodationTreeNode("B"));
+							node_2.add(new AccomodationTreeNode("C"));
 						node_1.add(node_2);
 					add(node_1);
-					node_1 = new DefaultMutableTreeNode("Fields");
-						node_2 = new DefaultMutableTreeNode("Small");
-							node_2.add(new DefaultMutableTreeNode("A"));
-							node_2.add(new DefaultMutableTreeNode("B"));
+					node_1 = new AccomodationTreeNode("Fields");
+						node_2 = new AccomodationTreeNode("Small");
+							node_2.add(new AccomodationTreeNode("A"));
+							node_2.add(new AccomodationTreeNode("B"));
 						node_1.add(node_2);
-						node_2 = new DefaultMutableTreeNode("Medium");
-							node_2.add(new DefaultMutableTreeNode("A"));
-							node_2.add(new DefaultMutableTreeNode("B"));
+						node_2 = new AccomodationTreeNode("Medium");
+							node_2.add(new AccomodationTreeNode("A"));
+							node_2.add(new AccomodationTreeNode("B"));
 						node_1.add(node_2);
-						node_2 = new DefaultMutableTreeNode("Big");
-							node_2.add(new DefaultMutableTreeNode("A"));
+						node_2 = new AccomodationTreeNode("Big");
+							node_2.add(new AccomodationTreeNode("A"));
 						node_1.add(node_2);
-						node_2 = new DefaultMutableTreeNode("Caravan");
-							node_2.add(new DefaultMutableTreeNode("A"));
+						node_2 = new AccomodationTreeNode("Caravan");
+							node_2.add(new AccomodationTreeNode("A"));
 						node_1.add(node_2);
 					add(node_1);
 				}
-			}
-		));
+			};
+		tree.setModel(new DefaultTreeModel(selectedNode));
 		tree.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);;
 		tree.setCellRenderer(new CampingTreeCellRenderer());
 		splitPane.setLeftComponent(tree);
@@ -150,15 +156,15 @@ public class PanelAccomodation extends MainPanel {
 		splitPane.setRightComponent(subSplit);
 		
 		JScrollPane sReservations = new JScrollPane();
-		sReservations.setPreferredSize(new Dimension(2, 350));
+		sReservations.setPreferredSize(new Dimension(2, 300));
 		subSplit.setLeftComponent(sReservations);
 		
 		JPanel pnlDetails = new JPanel();
 		subSplit.setRightComponent(pnlDetails);
 		GridBagLayout gbl_pnlDetails = new GridBagLayout();
-		gbl_pnlDetails.columnWidths = new int[]{69, 54, 31, 51, 0};
+		gbl_pnlDetails.columnWidths = new int[]{54, 31, 51, 0};
 		gbl_pnlDetails.rowHeights = new int[]{20, 0, 0, 0};
-		gbl_pnlDetails.columnWeights = new double[]{0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		gbl_pnlDetails.columnWeights = new double[]{0.0, 0.0, 1.0, Double.MIN_VALUE};
 		gbl_pnlDetails.rowWeights = new double[]{1.0, 0.0, 0.0, Double.MIN_VALUE};
 		pnlDetails.setLayout(gbl_pnlDetails);
 		
@@ -173,16 +179,15 @@ public class PanelAccomodation extends MainPanel {
 		GridBagConstraints gbc_lblFeatures = new GridBagConstraints();
 		gbc_lblFeatures.anchor = GridBagConstraints.NORTHEAST;
 		gbc_lblFeatures.insets = new Insets(0, 0, 5, 5);
-		gbc_lblFeatures.gridx = 2;
+		gbc_lblFeatures.gridx = 1;
 		gbc_lblFeatures.gridy = 0;
 		pnlDetails.add(lblFeatures, gbc_lblFeatures);
 		
 		detailSplit = new JSplitPane();
 		GridBagConstraints gbc_detailSplit = new GridBagConstraints();
 		gbc_detailSplit.gridheight = 3;
-		gbc_detailSplit.insets = new Insets(0, 0, 5, 0);
 		gbc_detailSplit.fill = GridBagConstraints.BOTH;
-		gbc_detailSplit.gridx = 3;
+		gbc_detailSplit.gridx = 2;
 		gbc_detailSplit.gridy = 0;
 		pnlDetails.add(detailSplit, gbc_detailSplit);
 		
@@ -193,6 +198,8 @@ public class PanelAccomodation extends MainPanel {
 		detailSplit.setLeftComponent(sText);
 		
 		txtFeatures = new JTextArea();
+		txtFeatures.addCaretListener(new TxtFeaturesCaretListener());
+		txtFeatures.setPreferredSize(new Dimension(200, 16));
 		txtFeatures.setTabSize(4);
 		txtFeatures.setLineWrap(true);
 		sText.setViewportView(txtFeatures);
@@ -201,16 +208,17 @@ public class PanelAccomodation extends MainPanel {
 		GridBagConstraints gbc_lblStatus = new GridBagConstraints();
 		gbc_lblStatus.anchor = GridBagConstraints.EAST;
 		gbc_lblStatus.insets = new Insets(0, 0, 0, 5);
-		gbc_lblStatus.gridx = 1;
+		gbc_lblStatus.gridx = 0;
 		gbc_lblStatus.gridy = 2;
 		pnlDetails.add(lblStatus, gbc_lblStatus);
 		
 		cmbStatus = new JComboBox<>();
+		cmbStatus.addItemListener(new CmbStatusItemListener());
 		cmbStatus.setModel(new DefaultComboBoxModel<>(EnumObjectStates.values()));
 		GridBagConstraints gbc_cmbStatus = new GridBagConstraints();
 		gbc_cmbStatus.anchor = GridBagConstraints.NORTHWEST;
 		gbc_cmbStatus.insets = new Insets(0, 0, 0, 5);
-		gbc_cmbStatus.gridx = 2;
+		gbc_cmbStatus.gridx = 1;
 		gbc_cmbStatus.gridy = 2;
 		pnlDetails.add(cmbStatus, gbc_cmbStatus);
 		
@@ -218,17 +226,18 @@ public class PanelAccomodation extends MainPanel {
 		GridBagConstraints gbc_lblPrice = new GridBagConstraints();
 		gbc_lblPrice.anchor = GridBagConstraints.EAST;
 		gbc_lblPrice.insets = new Insets(0, 0, 5, 5);
-		gbc_lblPrice.gridx = 1;
+		gbc_lblPrice.gridx = 0;
 		gbc_lblPrice.gridy = 1;
 		pnlDetails.add(lblPrice, gbc_lblPrice);
 		
 		spPrice = new JSpinner();
+		spPrice.addChangeListener(new SpPriceChangeListener());
 		spPrice.setModel(new SpinnerNumberModel(0.0f, 0.0f, null, 0.1f));
 		GridBagConstraints gbc_spPrice = new GridBagConstraints();
 		gbc_spPrice.fill = GridBagConstraints.HORIZONTAL;
 		gbc_spPrice.anchor = GridBagConstraints.NORTH;
 		gbc_spPrice.insets = new Insets(0, 0, 5, 5);
-		gbc_spPrice.gridx = 2;
+		gbc_spPrice.gridx = 1;
 		gbc_spPrice.gridy = 1;
 		pnlDetails.add(spPrice, gbc_spPrice);
 		
@@ -258,6 +267,31 @@ public class PanelAccomodation extends MainPanel {
 		table.getColumnModel().getColumn(6).setCellEditor(new SpinnerEditor(_spPrice));
 
 		sReservations.setViewportView(table);
+		
+		updateDetails();
+	}
+	
+	public void updateDetails() {
+		AccomodationTreeNode sn = getSelectedNode();
+		String[] ts = sn.getTags();
+		if (ts.length > 0) {
+			String nameTags = "";
+			for (int i = ts.length - 1; i >= 0; i--)
+				nameTags += ts[i] + ",";
+			lblTags.setText(nameTags.substring(0, nameTags.length() - 1));
+		} else
+			lblTags.setText("no selection");
+
+		txtFeatures.setText(sn.getFeatures());
+		cmbStatus.setSelectedItem(sn.getState());
+		spPrice.setValue(sn.getPrice());
+
+		sGallery.removeAll();
+		for (ImageIcon ic : sn.getImages()) {
+			JLabel lb = new JLabel("");
+			lb.setIcon(ic);
+			sGallery.add(lb);
+		}
 	}
 
 	@Override
@@ -272,11 +306,13 @@ public class PanelAccomodation extends MainPanel {
 			String[] tags = Arrays.stream(t.getPath().getPath()).map(x -> x.toString()).toArray(String[]::new);
 			mdl.setCurrentTags(tags);
 			mdl.filter();
+			selectedNode = (AccomodationTreeNode) tree.getLastSelectedPathComponent();
+			updateDetails();
 			getMain().log("Selected node: " + Arrays.stream(tags).collect(Collectors.joining(", ")));
 		}
 	}
 
-	public DefaultMutableTreeNode addObject(Object child) {
+	private DefaultMutableTreeNode addObject(Object child) {
 	    DefaultMutableTreeNode parentNode = null;
 	    TreePath parentPath = tree.getSelectionPath();
 
@@ -290,8 +326,8 @@ public class PanelAccomodation extends MainPanel {
 	    return addObject(parentNode, child, true);
 	}
 	
-	public DefaultMutableTreeNode addObject(DefaultMutableTreeNode parent, Object child, boolean shouldBeVisible) {
-		DefaultMutableTreeNode childNode = new DefaultMutableTreeNode(child);
+	private DefaultMutableTreeNode addObject(DefaultMutableTreeNode parent, Object child, boolean shouldBeVisible) {
+		DefaultMutableTreeNode childNode = new AccomodationTreeNode(child);
 		((DefaultTreeModel) tree.getModel()).insertNodeInto(childNode, parent, parent.getChildCount());
 
 		// Make sure the user can see the lovely new node.
@@ -299,6 +335,12 @@ public class PanelAccomodation extends MainPanel {
 			tree.scrollPathToVisible(new TreePath(childNode.getPath()));
 
 		return childNode;
+	}
+	
+	private AccomodationTreeNode getSelectedNode() {
+		if (selectedNode == null)
+			selectedNode = (AccomodationTreeNode) tree.getModel().getRoot();
+		return selectedNode;
 	}
 
 	private class NewNodeActionListener implements ActionListener {
@@ -323,12 +365,29 @@ public class PanelAccomodation extends MainPanel {
 					"Delete object", JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE);
 
 			if (v == JOptionPane.YES_OPTION) {
-					parentNode = (DefaultMutableTreeNode) (parentPath.getLastPathComponent());
-					if (parentNode != (DefaultMutableTreeNode) (tree.getModel().getRoot()))
-						((DefaultTreeModel) tree.getModel()).removeNodeFromParent(parentNode);
-					else
-						getMain().log("ERROR: trying to remove root node");
+				parentNode = (DefaultMutableTreeNode) (parentPath.getLastPathComponent());
+				if (parentNode != (DefaultMutableTreeNode) (tree.getModel().getRoot()))
+					((DefaultTreeModel) tree.getModel()).removeNodeFromParent(parentNode);
+				else
+					getMain().log("ERROR: trying to remove root node");
 			}
 		}
 	}
+
+	private class SpPriceChangeListener implements ChangeListener {
+		public void stateChanged(ChangeEvent arg0) {
+			getSelectedNode().setPrice((float) spPrice.getValue());
+		}
+	}
+	private class CmbStatusItemListener implements ItemListener {
+		public void itemStateChanged(ItemEvent arg0) {
+			getSelectedNode().setState((EnumObjectStates) cmbStatus.getSelectedItem());
+		}
+	}
+	private class TxtFeaturesCaretListener implements CaretListener {
+		public void caretUpdate(CaretEvent arg0) {
+			getSelectedNode().setFeatures(txtFeatures.getText());
+		}
+	}
+
 }
